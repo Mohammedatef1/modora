f4b139d4f64e07aa4e6968bd7d9f42f7a3dd50a2
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-03-04 21:48:00 +0200

    debounce add to cart

--- src/components/eCommerce/Product.tsx
+++ src/components/eCommerce/Product.tsx
@@ -1,13 +1,31 @@
 import { addToCart } from "@store/cart/cartSlice";
 import { useAppDispatch } from "@store/hooks";
+import { useEffect, useState } from "react";
 import { TProduct } from "src/customTypes/product";
 
 
 const Product = ({id, img , price , title}: TProduct) => {
   const dispatch = useAppDispatch()
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [isBtnClicked, setIsBtnClicked] = useState(0);
+
+  useEffect(() => {
+    if(!isBtnClicked){
+      return
+    }
+    setIsDisabled(true)
+    const debounce = setTimeout(() => {
+      setIsDisabled(false)
+    }, 300);
+
+    return () => clearTimeout(debounce)
+  }, [isBtnClicked])
+
   const addToCartHandler = ()=>{
-    dispatch(addToCart(id))
+    dispatch(addToCart(id));
+    setIsBtnClicked(prev => prev + 1)
   }
+
   return (  
   <div product-id={`${id}`} className="flex flex-col items-center gap-4">
     <div className="aspect-[1/1.2] w-full">
@@ -15,7 +33,7 @@ const Product = ({id, img , price , title}: TProduct) => {
     </div>
     <p className="text-lg">{title}</p>
     <p className="text-lg">{price}</p>
-    <button onClick={addToCartHandler} className="bg-primary rounded-md px-6 py-2 text-lg text-background font-semibold">Add to cart</button>
+    <button disabled={isDisabled} onClick={addToCartHandler} className="bg-primary rounded-md px-6 py-2 text-lg text-background font-semibold transition-opacity disabled:opacity-60">Add to cart</button>
   </div> );
 }
  
