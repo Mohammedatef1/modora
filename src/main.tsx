6891a922d6e98e9194ef9c4bd954ef031d54bf8d
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-03-04 17:04:11 +0200

    add: cache cart using redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import AppRouter from '@routes/AppRouter'
 import '@styles/main.css'
 import { createRoot } from 'react-dom/client'
-import { store } from '@store/store'
+import { persistor, store } from '@store/store'
 import {Provider} from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <AppRouter/>
+    <PersistGate persistor={persistor} loading={null} >
+      <AppRouter/>
+    </PersistGate>
   </Provider>
 )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import categoriesSlice from '@store/categories/categoriesSlice'
 import productsSlice from '@store/products/productsSlice'
 import cartSlice from '@store/cart/cartSlice'
+import storage from 'redux-persist/lib/storage'
+import {persistReducer, persistStore} from "redux-persist"
+
+const cartPersistConfig = {
+  key: 'cart',
+  storage,
+  whitelist: ['items']
+}
+
+const rootReducer = combineReducers({
+  categories : categoriesSlice,
+  products: productsSlice,
+  cart: persistReducer(cartPersistConfig, cartSlice)
+})
 
 export const store = configureStore({
-  reducer: {
-    categories : categoriesSlice,
-    products: productsSlice,
-    cart: cartSlice
-  }
+  reducer: rootReducer
 })
+
+export const persistor = persistStore(store)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
