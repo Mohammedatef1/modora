bcf860bf3c1ea6540ffe0187f67bfe1bdaae439e
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-03-04 17:39:42 +0200

    add: animation to cart total

--- src/components/eCommerce/ShopingCart.tsx
+++ src/components/eCommerce/ShopingCart.tsx
@@ -1,16 +1,28 @@
 import { useAppSelector } from '@store/hooks';
 import Logo from '../../assets/svg/cart.svg?react'
 import { getTotalCartQuantity } from '@store/cart/selectors';
+import { useEffect, useState } from 'react';
 
 const ShoppingCart = () => {
 
   const totalQuantity = useAppSelector(getTotalCartQuantity)
+
+  const [isAnimated, setIsAnimated] = useState(false)
+  useEffect(() =>{
+    if (totalQuantity == 0) return;
+    setIsAnimated(true);
+    const delay = setTimeout(() => {
+      setIsAnimated(false)
+    }, 300)
+
+    return () => clearTimeout(delay)
+  }, [totalQuantity])
   
 
   return ( 
   <div className='relative'>
     <Logo></Logo>
-    <div className='p-1 bg-primary rounded-full aspect-square absolute -top-3 -right-3 flex items-center justify-center h-6 text-sm'>{totalQuantity}</div>
+    <div className={` ${isAnimated? 'pulseAnimation' : ''} p-1 bg-primary rounded-full aspect-square absolute -top-3 -right-3 flex items-center justify-center h-6 text-sm`}>{totalQuantity}</div>
   </div> );
 }
  
