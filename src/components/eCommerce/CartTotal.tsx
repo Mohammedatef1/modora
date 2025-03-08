9d5c33efc4330abe989e18d641501865b2a4b2d5
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-03-09 00:52:10 +0200

    add: cart subtotal

--- src/components/eCommerce/CartTotal.tsx
+++ src/components/eCommerce/CartTotal.tsx
@@ -3,7 +3,7 @@ import { useAppSelector } from "@store/hooks"
 const CartTotal = () => {
   const {products} = useAppSelector(state => state.cart);
   
-  const totalCartPrice = products.reduce((previousValue, el) => {
+  const cartSubTotal = products.reduce((previousValue, el) => {
     const price = el.price;
     const quantity = el.quantity || 1;
     if(quantity && typeof quantity == "number"){
@@ -15,7 +15,7 @@ const CartTotal = () => {
   return (
     <div className="flex items-center justify-between py-3">
       <h3 className="font-bold">Subtotal:</h3>
-      <h3>{totalCartPrice.toFixed(2)} EGP</h3>
+      <h3>{cartSubTotal.toFixed(2)} EGP</h3>
     </div>
   )
 }
