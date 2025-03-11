e11cd355c66f75b504a9e828d6c6915932763b06
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-03-11 20:57:05 +0200

    refactor: heading

--- src/components/common/Heading.tsx
+++ src/components/common/Heading.tsx
@@ -1,7 +1,6 @@
 import { memo } from "react"
 
 const Heading = memo(({title} : {title : string}) => {
-  console.log('renders')
   return (
     <h2 className="mb-6 font-bold text-2xl">{title}</h2>
   )
