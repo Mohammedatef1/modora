23d7b2b1bbc251d437947832b54b1a9410d76ec1
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-03-11 23:34:27 +0200

    update: category

--- src/components/eCommerce/Category.tsx
+++ src/components/eCommerce/Category.tsx
@@ -3,12 +3,12 @@ import { TCategories } from "src/customTypes/category";
 
 const Category = ({title , prefix , img}: TCategories) => {
   return ( 
-  <div className="flex flex-col items-center gap-y-4">
-    <Link to={`/categories/products/${prefix}`}>
+  <div>
+    <Link className="group flex flex-col items-center gap-y-4" to={`/categories/products/${prefix}`}>
       <div className="aspect-square w-full">
         <img src={img} loading="lazy" alt={title} className="w-full h-full rounded-full" />
       </div>
-      <p className="text-lg">{title}</p>
+      <p className="text-lg font-semibold text-center group-hover:underline">{title}</p>
     </Link>
   </div> );
 }
