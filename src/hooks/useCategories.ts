d8bbf1f5b49b8814d77d8d579907025ed717efcf
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-03-11 22:14:45 +0200

    add: hook to categories

---
+++ src/hooks/useCategories.ts
@@ -0,0 +1,18 @@
+import actGetCategories from "@store/categories/actions/getAllCategories";
+import { cleanUpCategories } from "@store/categories/categoriesSlice";
+import { useAppDispatch, useAppSelector } from "@store/hooks";
+import { useEffect } from "react";
+
+const useCategories = () => {
+  const dispatch = useAppDispatch()
+  const {error, loading , records} = useAppSelector(state => state.categories);
+
+  useEffect(()=>{
+    dispatch(actGetCategories())
+    return () => {dispatch(cleanUpCategories())} 
+  },[dispatch])
+
+  return {error, loading , records}
+}
+
+export default useCategories
--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -2,20 +2,10 @@ import GridList from "@components/common/GridList";
 import Heading from "@components/common/Heading";
 import Category from "@components/eCommerce/Category";
 import Loading from "@feedback/Loading";
-import actGetCategories from "@store/categories/actions/getAllCategories";
-import { cleanUpCategories } from "@store/categories/categoriesSlice";
-import { useAppDispatch, useAppSelector } from "@store/hooks";
-import { useEffect } from "react";
+import useCategories from "@hooks/useCategories";
 
 const Categories = () => {
-  const dispatch = useAppDispatch()
-  const {error, loading , records} = useAppSelector(state => state.categories);
-  
-
-  useEffect(()=>{
-    dispatch(actGetCategories())
-    return () => {dispatch(cleanUpCategories())} 
-  },[dispatch])
+  const {error, loading , records} = useCategories()
 
   return ( 
     <>
