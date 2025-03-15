e96d228dba24580aa3495e0099fbf122aec0454c
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-03-16 00:59:57 +0200

    update: available email message

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -10,10 +10,10 @@ interface InputProps<TFieldValue extends FieldValues>{
   onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void ;
   loadingText?: string;
   disabled?: boolean;
-  isAvailable?: boolean
+  availableText?: string
 } 
 
-const Input = <TFieldValue extends FieldValues>({label, type = "text", register, name, error, onBlur, loadingText, disabled, isAvailable} : InputProps<TFieldValue> )  => {
+const Input = <TFieldValue extends FieldValues>({label, type = "text", register, name, error, onBlur, loadingText, disabled, availableText} : InputProps<TFieldValue> )  => {
   const onBlurHandler = (e: React.FocusEvent<HTMLInputElement>) => {
     if(onBlur){
       onBlur(e);
@@ -26,9 +26,10 @@ const Input = <TFieldValue extends FieldValues>({label, type = "text", register,
   return (
     <div className="flex flex-col gap-y-px">
       <label className="text-sm md:text-base mb-0.5 text-slate-700">{label}</label>
-      <input type={type} className={`px-3 py-1 text-sm md:text-base border rounded-sm bg-gray-50 ${error ? 'border-red-500 outline-red-500' : isAvailable ? 'border-green-500': 'border-gray-300'}`} {...register(name)} onBlur={onBlurHandler} disabled={disabled}/>
+      <input type={type} className={`px-3 py-1 text-sm md:text-base border rounded-sm bg-gray-50 ${error ? 'border-red-500 outline-red-500' : availableText ? 'border-green-500': 'border-gray-300'}`} {...register(name)} onBlur={onBlurHandler} disabled={disabled}/>
       {error && <span className="text-red-500 text-sm">{error}</span>}
       {loadingText && <span className="text-sm">{loadingText}</span>}
+      {availableText && !error && <span className="text-green-600 text-sm">{availableText}</span>}
     </div>
   )
 }
