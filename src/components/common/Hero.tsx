50487c55d8d91168043d084d7b7dd0c0bda0beda
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-03-22 22:11:09 +0200

    update: hero section

--- src/components/common/Hero.tsx
+++ src/components/common/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <div className="pt-16 p-10 rounded-xl bg-light-gold relative z-1 md:max-w-xl">
           <p className="text-sm md:text-base font-semibold">New Arrival</p>
           <h2 className="text-primary font-bold text-3xl md:text-4xl lg:text-5xl mt-1">Discover Our New Collection</h2>
-          <p className="text-base md:text-lg mt-3 font-semibold">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+          <p className="text-base md:text-lg mt-3 font-semibold">Upgrade your space with our newest designs, crafted for style, comfort, and durability. Find the perfect pieces to transform your home.</p>
           <button className="py-4 lg:py-5 px-11 md:px-14 lg:px-16 text-sm md:text-base text-background font-bold bg-primary text-uppercase mt-7 md:mt-11">Buy Now</button>
       </div>
       </div>
