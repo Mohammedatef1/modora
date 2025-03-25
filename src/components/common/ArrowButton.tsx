7351042402de09c77995dfb8cd502fbcd5807c86
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-03-25 22:13:39 +0200

    update: feature highlight

--- src/components/common/ArrowButton.tsx
+++ src/components/common/ArrowButton.tsx
@@ -4,11 +4,12 @@ import Arrow from "@assets/svg/arrow.svg?react"
 interface ArrowButtonProps {
   label: string;
   link?: string;
+  className? :string;
 }
 
-const ArrowButton = ({label, link="/"} : ArrowButtonProps) => {
+const ArrowButton = ({label, link="/", className} : ArrowButtonProps) => {
   return (
-    <Link to={link} className="text-xs md:text-sm text-primary flex items-center gap-3">
+    <Link to={link} className={`text-xs md:text-sm text-primary flex items-center gap-3 ${className}`}>
       <span>{label}</span>
       <Arrow className="text-primary fill-primary stroke-primary"/>
     </Link>
--- src/components/common/FeatureHighlight.tsx
+++ src/components/common/FeatureHighlight.tsx
@@ -1,3 +1,5 @@
+import ArrowButton from "./ArrowButton";
+
 interface FeatureHighlightProps {
   title: string;
   subtitle: string;
@@ -5,16 +7,25 @@ interface FeatureHighlightProps {
   imgSrc: string;
   ctaLabel: string;
   ctaLink: string;
-  backgroundColor?: string;
+  mode?: "standard" | "compact";
 }
 
-const FeatureHighlight = ({ctaLabel, ctaLink, description, imgSrc, subtitle, title, backgroundColor} : FeatureHighlightProps) => {
+const FeatureHighlight = ({ctaLabel, ctaLink, description, imgSrc, subtitle, title, mode="standard"} : FeatureHighlightProps) => {
   return (
-    <div className="flex py-12 md:py-40 lg:py-60">
+    <div className="flex flex-wrap gap-y-10 py-12 md:py-40 lg:py-60 lg:max-w-7xl lg:m-auto">
       <div className="w-full md:w-1/2">
-        <img src={imgSrc} alt="Experience image" width={455} height={629} className="w-10/12 h-auto rounded-e-2xl"/>
+        {mode === "standard"? (<img src={imgSrc} alt="Experience image" width={455} height={629} className="w-11/12 sm:w-10/12 h-auto rounded-e-2xl"/>) : 
+        (<div>
+          <img src="" width={450} height={629} alt="" />
+        </div>) }
       </div>
-    </div>
+      <div className={`w-full md:w-1/2 flex flex-col gap-y-5 p-4 sm:p-6 max-w-xl ${mode === "standard" ? 'ms:ps-20' : ''}`}>
+        <h3 className="text-primary text-base md:text-lg font-semibold">{subtitle}</h3>
+        <h2 className="text-2xl md:text-4xl font-bold">{title}</h2>
+        <p className="text-base md:text-lg">{description}</p>
+        <ArrowButton label={ctaLabel} link={ctaLink} className="mt-1"/>
+      </div>
+    </div> 
   )
 }
 
