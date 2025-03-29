9a2bbc47620d918fb7b2c40839117a82fa8918fa
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-03-29 05:59:33 +0200

    add: breadcrumb

---
+++ src/components/common/Breadcrumb.tsx
@@ -0,0 +1,30 @@
+import breadcrumbBackground from "@assets/images/breadcrumb background.png"
+import { Link } from "react-router-dom"
+import { capitalizeFirstLetter } from "src/utils"
+
+interface BreadcrumbProps {
+  path: string
+}
+
+const Breadcrumb = ({ path }: BreadcrumbProps) => {
+  const pathItems = path.split('/')
+  console.log(pathItems)
+  return (
+    <div className="relative h-56 md:h-72 lg:h-80 flex flex-col items-center justify-center text-center overflow-hidden">
+      <img src={breadcrumbBackground} alt="breadcrumb background image" className="w-full h-full object-cover absolute top-0 left-0 z-0 opacity-50 blur-sm" />
+      <div className="relative z-5 text-center">
+        <h3 className="font-semibold text-2xl md:text-4xl lg:text-5xl mb-3">{pathItems[pathItems.length -1]}</h3>
+        <div className="flex items-center justify-center gap-x-1.5">
+          {pathItems.map((item, index) => (
+            <>
+              {pathItems.length === index + 1 ? <p>{capitalizeFirstLetter(item)}</p> : <Link to={item === 'home' ? '/' :  item}>{capitalizeFirstLetter(item)}</Link> }
+              <span className="last:hidden font-semibold">{`>`}</span>
+            </>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Breadcrumb
--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,5 +1,5 @@
+import Breadcrumb from "@components/common/Breadcrumb";
 import GridList from "@components/common/GridList";
-import Heading from "@components/common/Heading";
 import Category from "@components/eCommerce/Category";
 import Loading from "@feedback/Loading";
 import useCategories from "@hooks/useCategories";
@@ -9,10 +9,12 @@ const Categories = () => {
 
   return ( 
     <>
-      <Heading title="Categories" />
-      <Loading type="category" error={error} loading={loading}>
-        <GridList records={records} renderedElementFunction={(record) => (<Category {...record} key={record.id}/>)}/>
-      </Loading>
+      <Breadcrumb path="home/categories"/>
+      <div className="max-w-7xl mx-auto p-4 sm:p-6">
+        <Loading type="category" error={error} loading={loading}>
+          <GridList records={records} renderedElementFunction={(record) => (<Category {...record} key={record.id}/>)}/>
+        </Loading>
+      </div>
     </> );
 }
  
