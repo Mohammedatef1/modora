6df93576b4dc35ef1529bfc2760e328b4b098b22
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-03-29 06:09:12 +0200

    upsate: cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,4 +1,4 @@
-import Heading from "@components/common/Heading"
+import Breadcrumb from "@components/common/Breadcrumb"
 import ListCartItems from "@components/common/ListCartItems"
 import CartItem from "@components/eCommerce/CartItem"
 import CartTotal from "@components/eCommerce/CartTotal"
@@ -10,13 +10,15 @@ const Cart = () => {
   
   return (
     <>
-      <Heading title="Cart" />
-      <Loading error={cartError} type="cart" loading={cartLoading}><ListCartItems records={products} renderedItemFunction={(product) => (<CartItem key={product.id} {...product}></CartItem>)}></ListCartItems></Loading>
-      <CartTotal></CartTotal>
-      {accessToken && <div className="flex flex-col items-center">
-        <button onClick={placeOrderHandler} disabled={ordersLoading === "pending"} className="ms-auto bg-blue-500 px-4 py-2 rounded-sm text-white disabled:opacity-75 transition-opacity">Place order</button>
-      </div>}
-      {ordersError && <p className="w-fit ms-auto text-red-500">{ordersError}</p>}
+      <Breadcrumb path="home/cart"/>
+      <div className="max-w-7xl mx-auto p-4 sm:p-6">
+        <Loading error={cartError} type="cart" loading={cartLoading}><ListCartItems records={products} renderedItemFunction={(product) => (<CartItem key={product.id} {...product}></CartItem>)}></ListCartItems></Loading>
+        <CartTotal></CartTotal>
+        {accessToken && <div className="flex flex-col items-center">
+          <button onClick={placeOrderHandler} disabled={ordersLoading === "pending"} className="ms-auto bg-blue-500 px-4 py-2 rounded-sm text-white disabled:opacity-75 transition-opacity">Place order</button>
+        </div>}
+        {ordersError && <p className="w-fit ms-auto text-red-500">{ordersError}</p>}
+      </div>
     </>
   )
 }
