f6c1e8d46604573313012db24447317aeb48481b
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-04-29 23:05:43 +0300

    add: full pading option

--- src/components/common/MaxWidthWrapper.tsx
+++ src/components/common/MaxWidthWrapper.tsx
@@ -4,16 +4,18 @@ interface MaxWidthWrapperProps {
   children: React.ReactNode;
   className?: string;
   type?: keyof JSX.IntrinsicElements;
+  fullPadding?: boolean
 }
 
 const MaxWidthWrapper: React.FC<MaxWidthWrapperProps> = ({
   children,
   className = '',
   type = 'div',
+  fullPadding= false
 }) => {
   const Component = type;
   return (
-    <Component className={`max-w-7xl mx-auto px-4 sm:px-6 ${className}`}>
+    <Component className={`max-w-7xl mx-auto ${fullPadding? 'p-4 sm:p-6' : 'px-4 sm:px-6'}  ${className}`}>
       {children}
     </Component>
   );
