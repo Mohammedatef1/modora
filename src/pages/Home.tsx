1ca7ca7d59937ff5cefa53eb84dffa78e8153cd1
Mohammed Atef <mohammed.atef.4200@gmail.com>
2025-06-13 23:42:23 +0300

    revert

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,8 +19,6 @@ const Home = () => {
   <div>
     <Hero />
 
-    Test
-
     <Suspense fallback={<div className="h-20 py-2"><Loader /></div>}>
       <Features />
       <BestSellingProducts tabs={['Chairs', 'Beds', 'Sofas', 'Lamps']} />
